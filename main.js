e838a6f2458d1e9c7d27307f7bd2cbf85a9742f7
Mars4all <88582613+Mars4all@users.noreply.github.com>
2023-05-01 09:49:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	// when document loaded:
+   
+    
+
+	
+        $("#test_alert").on("mousedown", function() {
+            alert("Guten Tag! Wir sind ein Familienunternehmen in der Rohrverstopfungsbeseitigungsbranche und befinden uns bereits in der zweiten Generation.\n\nSeit vielen Jahren bieten wir unseren Kunden einen Rundum-Service rund um die Dienstleistung der Rohrverstopfungsbeseitigung an.\n\nUnser Markenzeichen ist ein hervorragendes Preis-Leistungs-Verhältnis. Wir sind uns bewusst, dass unsere Kunden in der Regel unerwartete Kosten für Reparaturen und Dienstleistungen scheuen und daher legen wir besonderen Wert darauf, unseren Kunden faire Preise anzubieten, ohne dabei an der Qualität unserer Arbeit zu sparen.\n\nUnsere Bewertungen sprechen eine deutliche Sprache. Wir sind stolz darauf, dass unsere Kunden mit unserem Service zufrieden sind und uns weiterempfehlen. Für uns ist es wichtig, dass jeder Kunde zufrieden unser Unternehmen verlässt und sich darauf verlassen kann, dass seine Rohrverstopfung schnell und zuverlässig beseitigt wird.\n\nAls Familienunternehmen legen wir großen Wert auf eine persönliche Kundenbeziehung. Wir sind stolz darauf, dass viele unserer Kunden uns seit Jahren treu sind und uns weiterempfehlen. Wir setzen alles daran, diese enge Beziehung zu unseren Kunden aufrechtzuerhalten und auch in Zukunft zu vertiefen.\n\nWir sind bereit, allen Kunden einen professionellen und zuverlässigen Service anzubieten. Kontaktieren Sie uns, um herauszufinden, wie wir Ihnen bei der Beseitigung Ihrer Rohrverstopfung helfen können!");
+        });
+    
+    
+
+    
+
+
+	
+
+
+
+});
